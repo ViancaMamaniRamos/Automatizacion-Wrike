0eaa662cf74e624e8535e53bf6d1b8761ded289a
ViancaMamaniRamos <viancamamaniramos@gmail.com>
2024-09-19 09:29:13 -0400

    fix index.js

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,16 +1,6 @@
+import './commands'
 Cypress.on('uncaught:exception', (err, runnable) => {
-    // Ignora los errores específicos de "NoSuchMethodError" y "Cannot read properties of null"
-    if (err.message.includes('NoSuchMethodError') || err.message.includes('Cannot read properties of null')) {
-      return false; // Evita que Cypress falle por estos errores específicos
-    }
-    
-    // Ignora el error de "ResizeObserver loop completed with undelivered notifications"
-    if (err.message.includes('ResizeObserver loop completed with undelivered notifications')) {
-      return false; // Evita que Cypress falle por este error específico
-    }
-  
-    // O simplemente ignora todas las excepciones (como ya lo tienes implementado)
-    return false; 
-  });
-  
+  // returning false here prevents Cypress from failing the test
+  return false
+})
   
